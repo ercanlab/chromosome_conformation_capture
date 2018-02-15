c137e32281d8c0da28475943ffa6ea80d3b7672b
Matt Paul <matthew.paul.2006@gmail.com>
2018-02-15 17:54:12 -0500

    Update merge_interacting_reads.pl

--- merge_interacting_reads.pl
+++ merge_interacting_reads.pl
@@ -1,22 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 
-#################################
-#    Merge Interacting Reads    #
-#################################
-
-# This script was written by Anna-Lena Kranz
-# This script will take two bed files and look for matching paired ends. It will then output a paired end bed file for all reads that have 
-# a partner.
-
-# Required inputs
-#A-bed file 1. 
-#B-bed file 1. 
-
-#Example input for NYU HPC:
-##perl /home/mrp420/yeast/scripts/merge_interacting_reads.pl MERGEREADY${TAG2}.bed MERGEREADY${TAG4}.bed ${TAG5}_MERGED.bed
-
-#Read in my bed files
+#Input files
 my $file1 = shift();
 my $file2 = shift();
 my $output = shift();
@@ -29,39 +14,19 @@ my %reads;
 
 my $count = 0;
 
-#This looks for the pairs. It is based on using the identifier SRR* at the start of each unique ID for sequencing runs as is standard for 
-# GEO. If different identifiers are used in the bed files, then modify the SRR section of the script. 
-
+#Breakdown file 1
 while (<INPUT1>) {
     chomp();
-#    my $input2 = <INPUT2>;
-#    chomp($input2);
     if (/^(SRR.*?)\t/) {
         my $header1 = $1;
         my @line1 = split("\t", $_);
         unless ($reads{$header1}) {
             $reads{$header1} = $line1[1]."\t".$line1[2]."\t".$line1[3];
-        } else {
-            print "again: $header1\n"; exit(0);
         }
-#        my $header2;
-#        if ($input2 =~ /^(SRR0381.*?)\s+/) {
-#            $header2 = $1;
-#        }
-#        my @line1 = split("\t", $_);
-#        my @line2 = split("\t", $input2);
-#        unless ($header1 eq $header2) {
-#            print "Problem in line $.: $line1[0] - $line2[0]!\n";
-#            exit(0);
-#        }
-#        if ($line1[2] =~ /^chr/ && $line2[2] =~ /^chr/) {
-#            print OUTPUT "$line1[0]\t$line1[2]\t$line1[3]\t$line2[0]\t$line2[2]\t$line2[3]\n";
-#        }
-    }   
+    }
 }
 
-#print scalar(keys(%reads))."\n";
-
+#Breakdown file 2 and output matches
 while (<INPUT2>) {
     chomp();
     if (/^(SRR.*?)\t/) {
@@ -71,7 +36,11 @@ while (<INPUT2>) {
             $count++;
             print OUTPUT "$header2\t".$reads{$header2}."\t$line2[1]\t$line2[2]\t$line2[3]\n";;
         }
-    }  
+    }
 }
 
-#print scalar(keys(%reads))."\n";
+print $count." reads found in both pairs.\n";
+
+close INPUT1;
+close INPUT2;
+close OUTPUT;
